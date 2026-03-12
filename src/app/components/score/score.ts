a7167bc56372418ef7e17383d57d812039f7285b
Dorian Weber <dorianweb57@gmail.com>
2026-03-12 10:48:30 +0100

    feat(score): ajout du composant score ainsi que la methode qui permet dl'incrementation

--- src/app/components/score/score.ts
+++ src/app/components/score/score.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {Letters} from '../../services/letters';
+import {Words} from '../../services/words';
 
 @Component({
   selector: 'app-score',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './score.css',
 })
 export class Score {
+  private letters = inject(Letters);
+  private word = inject(Words);
 
+  protected lastLetter = this.letters.getLastLetter
+  protected getRandomWord = this.word.getRandomWord()
+
+  protected counter = signal(0);
+
+  scoreCounter() : void {
+    if(!this.getRandomWord.includes(this.lastLetter()!)) {
+      this.counter.update(count => count + 1);
+
+    }
+  }
 }
