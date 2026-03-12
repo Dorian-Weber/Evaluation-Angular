52faec28bde29a1674a340e95f5614ce621ca73a
Dorian Weber <dorianweb57@gmail.com>
2026-03-12 09:33:21 +0100

    feat(sidebar): ajout des badges pour les lettres valide ou non

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,17 +1,23 @@
 import {Component, inject} from '@angular/core';
 import {Letters} from '../../services/letters';
+import {Words} from '../../services/words';
+import {NgClass} from '@angular/common';
 
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 
 export class Sidebar {
   private letters = inject(Letters);
+  private word = inject(Words);
 
   lettersList = this.letters.getLettersList
+  getRandomWord = this.word.getRandomWord()
 
 }
