f62e31ae009d8d45dd5e14630de3bb2bc6d21a86
Dorian Weber <dorianweb57@gmail.com>
2026-03-13 14:44:00 +0100

    feat(game-over): ajout de l'écoute du score pour set la partie en lose ou win

--- src/app/components/game-over/game-over.ts
+++ src/app/components/game-over/game-over.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
+import {GameService} from '../../services/game-service';
+import {Error} from '../../services/error';
+import {Words} from '../../services/words';
+import {Letters} from '../../services/letters';
 
 @Component({
   selector: 'app-game-over',
@@ -7,6 +11,31 @@ import { Component } from '@angular/core';
   styleUrl: './game-over.css',
 })
 export class GameOver {
+
+  private errorService = inject(Error);
+  private gameService= inject(GameService);
+  private wordsService = inject(Words);
+  private lettersService= inject(Letters);
+
+  private error$ = this.errorService.counter$
+  private testLetterAgainstWord = this.wordsService.testLetterAgainstWord
+  private lettersList$ = this.lettersService.lettersList;
+
+
+  constructor() {
+    effect(() => {
+      if (this.error$() >= 5) {
+        this.gameService.setLose()
+        return;
+      };
+      if (this.testLetterAgainstWord(this.lettersList$())) {
+        this.gameService.setWin()
+        return;
+      }
+    })
+  }
+
+
 
 
 }
--- src/app/services/game-service.ts
+++ src/app/services/game-service.ts
@@ -1,22 +1,27 @@
-import {inject, Injectable, signal} from '@angular/core';
-import {Score} from '../components/score/score';
-import {Error} from './error';
+import {Injectable, signal} from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
 
-  private error = inject(Error);
-
-  private counter = this.error.counter$()
-  gameState = signal<'playing' | 'win' | 'lose'>('playing');
+  gameState$ = signal<'playing' | 'win' | 'lose'>('playing');
 
   setWin() {
-    this.gameState.set('win')
+    this.gameState$.set('win')
+    console.log(this.gameState$())
   }
 
   setLose() {
-    this.gameState.set('lose')
+    this.gameState$.set('lose')
+    console.log(this.gameState$())
   }
+
+  setPlaying() {
+    this.gameState$.set('playing')
+    console.log(this.gameState$())
+  }
+
+
 }
